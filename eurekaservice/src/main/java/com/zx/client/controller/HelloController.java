84d281b89b19b6672e7592e90362a013f527be2a
zhangxiang <863413076@qq.com>
2019-03-10 13:42:19 +0800

    配置中心搭建

--- eurekaservice/src/main/java/com/zx/client/controller/HelloController.java
+++ eurekaservice/src/main/java/com/zx/client/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${server.port}")
+    //@Value("${server.port}")
     private int port;
 
     @RequestMapping("index")
